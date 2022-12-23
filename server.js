e659198b51cb43cd75aae9341810144c072c0c24
Thayna <thaynas.s502@gmail.com>
2022-12-22 21:11:29 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("index")
+})
+
+app.get("/login", function(req, res){
+    res.render("login")
+})
+
+app.listen(8080);
+console.log("Rodando")
